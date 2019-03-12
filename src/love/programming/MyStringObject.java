de3f7d38afcce3b22e7507bd20c047bd22fde168
SrabaniS <srabani.suprakash@gmail.com>
2019-03-12 12:10:29 +0000

    my commit

---
+++ src/love/programming/MyStringObject.java
@@ -0,0 +1,12 @@
+package love.programming;
+
+public class MyStringObject {
+    public static void main(String[] args) {
+        String s1 = new String("Selenium Training ");
+//using new word
+        String s2 = "Seleinum Training";
+
+//creating object with ""
+
+
+    }}
